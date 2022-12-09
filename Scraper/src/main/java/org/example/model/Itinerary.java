2dbbb93b60a8a0d305d4a458115f906fc493864c
DavideBruni <davidebruni.005@outlook.it>
2022-12-09 18:21:08 +0100

    Add scraper and initiale dataset

---
+++ Scraper/src/main/java/org/example/model/Itinerary.java
@@ -0,0 +1,40 @@
+package org.example.model;
+
+public class Itinerary {
+    private String title;
+    private String subtitle;
+    private String description;
+    private int day;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+}
