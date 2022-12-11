fa354b4696fee6f515a0c1a1f959422749c38c10
DavideBruni <davidebruni.005@outlook.it>
2022-12-11 11:30:28 +0100

    Add classes

---
+++ Trip4Share/src/main/java/it/unipi/dii/lsmd/trip4share/ServletInitializer.java
@@ -0,0 +1,13 @@
+package it.unipi.dii.lsmd.trip4share;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Trip4ShareApplication.class);
+    }
+
+}
---
+++ Trip4Share/src/main/java/it/unipi/dii/lsmd/trip4share/Trip4ShareApplication.java
@@ -0,0 +1,13 @@
+package it.unipi.dii.lsmd.trip4share;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trip4ShareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Trip4ShareApplication.class, args);
+    }
+
+}
