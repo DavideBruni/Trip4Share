6687f73c324a015076dd323f52442e8b1774b735
Francesco Grillea <grilleafrancesco@gmail.com>
2022-12-14 12:23:04 +0100

    BaseDAOMongo and BaseDAORedis done

--- src/main/java/it/unipi/lsmd/dao/base/BaseDAOMongo.java
+++ src/main/java/it/unipi/lsmd/dao/base/BaseDAOMongo.java
@@ -1,7 +1,29 @@
 package it.unipi.lsmd.dao.base;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
 public abstract class BaseDAOMongo {
 
+    private static final String MONGO_HOST = "localhost";
+    private static final Integer MONGO_PORT = 27017;
+    private static final String MONGO_DB = "Trip4Share";
+    private static MongoClient client = null;
+
+    public static void init(){
+        String url = "mongodb://"+MONGO_HOST+":"+MONGO_PORT;
+        ConnectionString uri = new ConnectionString(url);
+        // TODO - add auth ?
+
+        client = MongoClients.create(uri);
+    }
+
+    public static MongoDatabase getConnection(){
+        MongoDatabase database = client.getDatabase(MONGO_DB);
+        return database;
+    }
 
 
 }
