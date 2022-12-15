c94a39955ed72083a7e5cf8f1bb18fad698d9c6a
Francesco Grillea <grilleafrancesco@gmail.com>
2022-12-15 12:23:32 +0100

    create session

--- src/main/java/it/unipi/lsmd/controller/LoginServlet.java
+++ src/main/java/it/unipi/lsmd/controller/LoginServlet.java
@@ -2,6 +2,7 @@ package it.unipi.lsmd.controller;
 
 import it.unipi.lsmd.dto.AuthenticatedUserDTO;
 import it.unipi.lsmd.service.impl.UserServiceImpl;
+import it.unipi.lsmd.utils.SecurityUtils;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -17,42 +18,62 @@ import java.util.Enumeration;
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
+    protected void processRequest(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+
+        System.out.println("Received: " + httpServletRequest.getMethod());
+
+        AuthenticatedUserDTO authenticatedUserDTO = SecurityUtils.getAuthenticatedUser(httpServletRequest);
+        UserServiceImpl userService = new UserServiceImpl();
+
+        // check if user is already authenticated
+        if(authenticatedUserDTO != null){
+            httpServletResponse.sendRedirect("user");
+            return;
+        }
+
+
+        if(httpServletRequest.getMethod().equals("GET")){
+
+            String targetJSP = "/WEB-INF/pages/login.jsp";
+            RequestDispatcher requestDispatcher = httpServletRequest.getRequestDispatcher(targetJSP);
+            requestDispatcher.forward(httpServletRequest, httpServletResponse);
+
+        }else if(httpServletRequest.getMethod().equals("POST")){
+            try {
+                String username = httpServletRequest.getParameter("username");
+                String password = httpServletRequest.getParameter("password");
+
+                if (username != null && password != null && !username.isEmpty() && !password.isEmpty()){
+                    authenticatedUserDTO = userService.authenticate(username, password);
+
+                    // set user as authenticated
+                    HttpSession session = httpServletRequest.getSession(true);
+                    session.setAttribute(SecurityUtils.AUTHENTICATED_USER_KEY, authenticatedUserDTO);
+
+                    // once logged in redirect to user's profile
+                    httpServletResponse.sendRedirect("user");
+
+                } else {
+                    httpServletRequest.setAttribute("errorMessage", "Invalid username or password.");
+                }
+            } catch (Exception e) {
+                //logger.error("Error during login operation.",e);
+                httpServletRequest.setAttribute("errorMessage", "Invalid username or password.");
+            }
+        }
+
+
+    }
 
     @Override
     protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
-        System.out.println("GET Operation");
-
-        String targetJSP = "/WEB-INF/pages/login.jsp";
-
-        RequestDispatcher requestDispatcher = httpServletRequest.getRequestDispatcher(targetJSP);
-        requestDispatcher.forward(httpServletRequest, httpServletResponse);
+        processRequest(httpServletRequest, httpServletResponse);
     }
 
 
     @Override
     protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
-        System.out.println("POST Operation");
-        String targetJSP = "WEB-INF/pages/login.jsp";
-
-        String username = httpServletRequest.getParameter("username");
-        String password = httpServletRequest.getParameter("password");
-
-        UserServiceImpl userService = new UserServiceImpl();
-        AuthenticatedUserDTO authenticatedUserDTO = null;
-
-        try {
-            if (username != null && password != null && !username.isEmpty() && !password.isEmpty()){
-                authenticatedUserDTO = userService.authenticate(username, password);
-                httpServletResponse.sendRedirect("user");
-
-            } else {
-                httpServletRequest.setAttribute("errorMessage", "Invalid username or password.");
-            }
-        } catch (Exception e) {
-            //logger.error("Error during login operation.",e);
-            httpServletRequest.setAttribute("errorMessage", "Invalid username or password.");
-        }
-
+        processRequest(httpServletRequest, httpServletResponse);
 
     }
 
---
+++ src/main/java/it/unipi/lsmd/utils/SecurityUtils.java
@@ -0,0 +1,19 @@
+package it.unipi.lsmd.utils;
+
+import it.unipi.lsmd.dto.AuthenticatedUserDTO;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.Enumeration;
+
+public class SecurityUtils {
+
+    public static final String AUTHENTICATED_USER_KEY = "authenticatedUser";
+
+    public static AuthenticatedUserDTO getAuthenticatedUser(HttpServletRequest request){
+        HttpSession session = request.getSession();
+        return (AuthenticatedUserDTO)session.getAttribute(AUTHENTICATED_USER_KEY);
+    }
+
+}
